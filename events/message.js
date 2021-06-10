cb9c2776b508c7b23d6137830402dc6022bd0fd4
Popcorn The Dev <79060182+Popcorn-The-Crappy-Dev@users.noreply.github.com>
2021-06-09 21:01:55 -0500

    Add files via upload

---
+++ events/message.js
@@ -0,0 +1,59 @@
+const config = require('../config.json');
+const Discord = require('discord.js');
+exports.run = async(client, message) => {
+    if (config.filter.enabled === true){
+        if (config.filter.disabledwords.some((messageword) => message.content.toLowerCase().includes(messageword))) {
+
+            message.delete();
+            const help = new Discord.MessageEmbed().setDescription(`You are not allowed to say that in this guild `).setTimestamp().setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`).setColor(config.embeds.color);
+            message.channel.send(help).then((messagess) => {
+                setTimeout(function () {
+                    messagess.delete();
+                }, 10000);
+            });
+            message.author.send(help);
+                }else {
+
+                }
+    }else {
+
+    }
+
+    if (config.disablediscordlinks === true) {
+        const help = ["discord.gg", "discord.com/invite/"];
+        if (help.some((messageword) => message.content.toLowerCase().includes(messageword))) {
+            message.delete();
+            const help = new Discord.MessageEmbed().setDescription("You are not allowed to use discord links in this guild").setTimestamp().setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`).setColor(config.embeds.color);
+            message.channel.send(help).then((messagess) => {
+                setTimeout(function () {
+                    messagess.delete();
+                }, 10000);
+            });
+            message.author.send(help);
+        }
+    } else {
+    
+    }
+    if(config['no ping'].enabled === true){
+        const help = config['no ping']['user ids(if enabled)']
+        if (help.some((messageword) => message.content.toLowerCase().includes(messageword))) {
+            const help = new Discord.MessageEmbed()
+            .setDescription(`Please don't ping me!`)
+            .setTimestamp()
+            .setImage(config['no ping']['image url'])
+            .setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`)
+            .setColor(config.embeds.color);
+            message.channel.send(help).then((messagess) => {
+                setTimeout(function () {
+                    messagess.delete();
+                }, 10000);
+            });
+            message.author.send(help);
+        }
+    }else {
+        
+    }
+};
+
+    
+
---
+++ events/ready.js
@@ -0,0 +1,9 @@
+
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+exports.run = async(client, message ) => {
+
+console.log(`Logged in as ${client.user.tag}!!`)
+console.log('\x1b[31m%s\x1b[0m',`Made by Popcorn The Crappy Dev#1001`)
+}
