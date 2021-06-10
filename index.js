db6b9281a9d6c5350eb39531108ef39f0bc3b83b
Brackzz <84167175+Brackzz@users.noreply.github.com>
2021-06-09 20:26:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const discord = require('discord.js');
+const fs = require('fs');
+const client = new discord.Client();
+let token = 'TOKEN_HERE';
+client.on('ready', () => {
+    console.log(`${client.user.tag} Has came online!`)
+    client.user.setActivity(config["main_config"].status)
+});
+client.on('message', message => {
+    if (message.content.includes()) {
+        message.delete()
+    } else if (message.content.includes('')) {
+        message.delete()
+    } else if (message.content.includes('')) {
+        message.delete()
+    } else if (message.content.includes('')) {
+        message.delete()
+    }
+    console.log(`Deleted a Message in ${message.guild.name}`)
+});
+client.login(token)
