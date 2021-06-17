2688766e955e347dd41bba867fcfd8a18d5b00ac
Popcorn The Dev <79060182+Popcorn-The-Crappy-Dev@users.noreply.github.com>
2021-06-17 04:01:51 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,23 +3,30 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client();
 
+
 client.aliases = new Discord.Collection();
 client.events = new Discord.Collection();
 
 
 
-fs.readdir('./events/', (err, files) => {
-    if (err) console.log(err);
-    files.forEach(file => {
-        let eventFunc = require(`./events/${file}`);
-        console.log("Successfully loaded " + file)
-        let eventName = file.split(".")[0];
-        client.on(eventName, (...args) => eventFunc.run(client, ...args));
-    });
+    fs.readdir('./events/', (err, files) => {
+        if (err) console.log(err);
+        files.forEach(file => {
+            let eventFunc = require(`./events/${file}`);
+            console.log("Successfully loaded " + file)
+            let eventName = file.split(".")[0];
+            client.on(eventName, (...args) => eventFunc.run(client, ...args));
+        });
 });
 
 
 
 
 
-client.login(config.main.token)
+
+
+
+
+
+
+client.login(config.token)
