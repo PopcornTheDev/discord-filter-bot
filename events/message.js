bf27d0b7fb2b372ea26791488816392e11b66fa7
Popcorn The Dev <79060182+Popcorn-The-Crappy-Dev@users.noreply.github.com>
2021-06-17 15:40:46 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,66 +1,66 @@
 const config = require('../config.json');
 const Discord = require('discord.js');
-exports.run = async(client, message) => {
-            if (config.filter.enabled === true) {
-                        config.filter.disabledwords.forEach(eachWord => {
-                            if (message.content.toLowerCase().search(eachWord.toLowerCase()) >= 0) {
-                                message.delete().catch();
-                                const help = new Discord.MessageEmbed().setDescription(`You are not allowed to say that in this guild `).setTimestamp().setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`).setColor(config.embeds.color);
-                                message.channel.send(help).then((messagess) => {
-                                    setTimeout(function () {
-                                        messagess.delete().catch();
-                                    }, 10000);
-                                });
-                                message.author.send(help).catch();
+exports.run = async (client, message) => {
+    if (config.filter.enabled === true) {
+        main.query(`SELECT * FROM staff_members WHERE user_id = "${message.author.id}"`, async function (err, result) {
+            if (!result[0]) {
+                config.filter.disabledwords.forEach(eachWord => {
+                    if (message.content.toLowerCase().search(eachWord.toLowerCase()) >= 0) {
+                        message.delete().catch();
+                        const help = new Discord.MessageEmbed().setDescription(`You are not allowed to say that in this guild `).setTimestamp().setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`).setColor(config.embeds.color);
+                        message.channel.send(help).then((messagess) => {
+                            setTimeout(function () {
+                                messagess.delete().catch();
+                            }, 10000);
+                        });
+                        message.author.send(help).catch();
                     }
                 });
             }
+        });
+    }
 
-            if (config.filter.disablediscordlinks === true) {
-                main.query(`SELECT * FROM staff_members WHERE user_id = "${message.author.id}"`, async function (err, result) {
-                    if (!result[0]) {
-                        const help = ["discord.gg", "discord.com/invite/"];
-                        help.forEach(eachWord => {
-                            if (message.content.toLowerCase().search(eachWord.toLowerCase()) >= 0) {
-                            message.delete().catch();
-                            const help = new Discord.MessageEmbed().setDescription("You are not allowed to use discord links in this guild").setTimestamp().setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`).setColor(config.embeds.color);
-                            message.channel.send(help).then((messagess) => {
-                                setTimeout(function () {
-                                    messagess.delete().catch();
-                                }, 10000);
-                            });
-                            message.author.send(help).catch();
-                        
+    if (config.filter.disablediscordlinks === true) {
 
-                    }
-                
+        const help = ["discord.gg", "discord.com/invite/"];
+        help.forEach(eachWord => {
+            if (message.content.toLowerCase().search(eachWord.toLowerCase()) >= 0) {
+                message.delete().catch();
+                const help = new Discord.MessageEmbed().setDescription("You are not allowed to use discord links in this guild").setTimestamp().setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`).setColor(config.embeds.color);
+                message.channel.send(help).then((messagess) => {
+                    setTimeout(function () {
+                        messagess.delete().catch();
+                    }, 10000);
                 });
+                message.author.send(help).catch();
+
+
             }
-            if (config['no ping'].enabled === true) {
-                main.query(`SELECT * FROM staff_members WHERE user_id = "${message.author.id}"`, async function (err, result) {
-                    if (!result[0]) {
-                        const help = config['no ping']['user ids(if enabled)']
-                        help.forEach(eachWord => {
-                            if (message.content.toLowerCase().search(eachWord.toLowerCase()) >= 0) {
-                            const help = new Discord.MessageEmbed()
-                                .setDescription(`Please don't ping me!`)
-                                .setTimestamp()
-                                .setImage(config['no ping']['image url'])
-                                .setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`)
-                                .setColor(config.embeds.color);
-                            message.channel.send(help).then((messagess) => {
-                                setTimeout(function () {
-                                    messagess.delete().catch();
-                                }, 10000);
-                            });
-                            message.author.send(help).catch();
-                            
-                    }
-                    
+
+        });
+    }
+    if (config['no ping'].enabled === true) {
+
+        const help = config['no ping']['user ids(if enabled)']
+        help.forEach(eachWord => {
+            if (message.content.toLowerCase().search(eachWord.toLowerCase()) >= 0) {
+                const help = new Discord.MessageEmbed()
+                    .setDescription(`Please don't ping me!`)
+                    .setTimestamp()
+                    .setImage(config['no ping']['image url'])
+                    .setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`)
+                    .setColor(config.embeds.color);
+                message.channel.send(help).then((messagess) => {
+                    setTimeout(function () {
+                        messagess.delete().catch();
+                    }, 10000);
                 });
-                
+                message.author.send(help).catch();
+
             }
-};
 
-    
+        });
 
+    }
+
+}
