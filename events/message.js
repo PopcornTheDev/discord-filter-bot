d8525851a4d228b8c08b210545d8daee2bbec2a3
Popcorn The Dev <79060182+Popcorn-The-Crappy-Dev@users.noreply.github.com>
2021-06-17 04:02:12 -0500

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -1,58 +1,57 @@
 const config = require('../config.json');
 const Discord = require('discord.js');
 exports.run = async(client, message) => {
-    if (config.filter.enabled === true){
-        if (config.filter.disabledwords.some((messageword) => message.content.toLowerCase().includes(messageword))) {
-
-            message.delete();
-            const help = new Discord.MessageEmbed().setDescription(`You are not allowed to say that in this guild `).setTimestamp().setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`).setColor(config.embeds.color);
-            message.channel.send(help).then((messagess) => {
-                setTimeout(function () {
-                    messagess.delete();
-                }, 10000);
-            });
-            message.author.send(help);
-                }else {
-
-                }
-    }else {
-
-    }
-
-    if (config.disablediscordlinks === true) {
-        const help = ["discord.gg", "discord.com/invite/"];
-        if (help.some((messageword) => message.content.toLowerCase().includes(messageword))) {
-            message.delete();
-            const help = new Discord.MessageEmbed().setDescription("You are not allowed to use discord links in this guild").setTimestamp().setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`).setColor(config.embeds.color);
-            message.channel.send(help).then((messagess) => {
-                setTimeout(function () {
-                    messagess.delete();
-                }, 10000);
-            });
-            message.author.send(help);
-        }
-    } else {
     
+    if(config['user(s)id(s)'].some(ids=> message.content.toLowerCase().includes(ids))){
+        message.delete()
+        const help = new Discord.MessageEmbed()
+        .setTitle(`Please Don't Ping Me`)
+        .setTimestamp()
+        .setFooter('No Ping | Made by Popcorn The Crappy Dev#1001')
+        .setColor('#d60d0d')
+        message.channel.send(help).then(messagess => {
+            setTimeout(function(){
+                messagess.delete()
+               },10000 )
+        })
+        message.author.send(help).then(messagess => {
+            setTimeout(function(){
+                messagess.delete()
+               },10000 )
+        })
     }
-    if(config['no ping'].enabled === true){
-        const help = config['no ping']['user ids(if enabled)']
-        if (help.some((messageword) => message.content.toLowerCase().includes(messageword))) {
-            const help = new Discord.MessageEmbed()
-            .setDescription(`Please don't ping me!`)
-            .setTimestamp()
-            .setImage(config['no ping']['image url'])
-            .setFooter(config.embeds["footer message"] || "Server Filter", `${message.author.avatarURL()}`)
-            .setColor(config.embeds.color);
-            message.channel.send(help).then((messagess) => {
-                setTimeout(function () {
-                    messagess.delete();
-                }, 10000);
-            });
-            message.author.send(help);
-        }
+    if(config.filter.disabled_words.some(messageword => message.content.toLowerCase().includes(messageword))){
+        message.delete()
+        const help = new Discord.MessageEmbed()
+        .setDescription(`You are not allowed to say that in this guild `)
+        .setTimestamp()
+        .setFooter('Server Filter', `${message.author.avatarURL()}`)
+        .setColor('#d60d0d')
+        message.channel.send(help).then(messagess => {
+            setTimeout(function(){
+                messagess.delete()
+               },10000 )
+        })
+        message.author.send(help)
+    }
+}   
+    if(config.filter.disablediscordlinks === 'false'){
     }else {
-        
-    }
+    const help = ["discord.gg", "discord.com/invite/"]
+    if(help.some(messageword => message.content.toLowerCase().includes(messageword))){
+    message.delete()
+    const help = new Discord.MessageEmbed()
+    .setDescription('You are not allowed to use discord links in this guild')
+    .setTimestamp()
+    .setFooter('Server Filter', `${message.author.avatarURL()}`)
+    .setColor('#d60d0d')
+    message.channel.send(help).then(messagess => {
+        setTimeout(function(){
+            messagess.delete()
+           },10000 )
+    })
+    message.author.send(help)
+}
 };
 
     
