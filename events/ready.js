c079cb7dee7a062c8aa729ca8ced8a2e5787709b
FAXES <31368070+FAXES@users.noreply.github.com>
2021-06-17 18:59:07 +1000

    Fix lol
    Fixes as this could execute without the bot actually already being started. Reduced to one console log line cause why have two?

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const config = require('../config.json');
 
 exports.run = async(client, message ) => {
-
-console.log(`Logged in as ${client.user.tag}!!`)
-console.log('\x1b[31m%s\x1b[0m',`Made by Popcorn The Crappy Dev#1001`)
+  	client.on("ready", async () => {
+    	console.log(`Logged in as ${client.user.tag}!!\n\x1b[31m%s\x1b[0m', Made by Popcorn The Crappy Dev#1001`)
+	});
 }
