e64b526f170214409b82e2d342fabf6e9fedb061
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-06-28 14:10:06 -0700

    Added presence options for the bot

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,14 @@ const Discord = require('discord.js');
 const config = require('../config.json');
 
 exports.run = async(client, message ) => {
-  	client.on("ready", async () => {
-    		console.log(`Logged in as ${client.user.tag}!!\n\x1b[31m%s\x1b[0m', Made by Popcorn The Crappy Dev#1001`)
+	console.log(`Logged in as ${client.user.tag}!!\n\x1b[31m%s\x1b[0m', Made by Popcorn The Crappy Dev#1001`)
+	
+	client.user.setPresence({
+ 		activity: {
+	    		name: config.main.presence_name,
+   			 type: config.main.presence_type
+  		},
+  		status: config.main.presence_status
 	});
+	
 }
