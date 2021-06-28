64c3296d2366449cef56e9f9d6b017ea7caf33e7
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-06-28 13:57:45 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ const config = require('../config.json');
 
 exports.run = async(client, message ) => {
   	client.on("ready", async () => {
-    	console.log(`Logged in as ${client.user.tag}!!\n\x1b[31m%s\x1b[0m', Made by Popcorn The Crappy Dev#1001`)
+    		console.log(`Logged in as ${client.user.tag}!!\n\x1b[31m%s\x1b[0m', Made by Popcorn The Crappy Dev#1001`)
 	});
 }
